61c8368dedf27bcbcb6d365e9363028afd4d749e
Alvin Lin <alvin.lin.dev@gmail.com>
2015-12-08 23:49:01 -0500

    stuffs

--- README.md
+++ README.md
@@ -1,3 +1,10 @@
 # creatopolis.sytes.net
 
 Web portal for the Creatopolis Minecraft server located at creatopolis.sytes.net
+
+# Setup for development
+```bash
+virtualenv env
+. ./activate
+pip install -r requirements.txt           
+```
--- app.py
+++ app.py
@@ -1,12 +1,22 @@
+# Web application
 from flask import Flask
+from flask import redirect, render_template
 from werkzeug.contrib.fixers import ProxyFix
+
+import sys
+
 app = Flask(__name__)
  
-@app.route('/')
-def hello():
-  return "Hello world!"
+@app.route("/")
+def index():
+  return render_template("index.html")
+
+@app.route("/*")
+def catchall():
+  return redirect("/")
  
 app.wsgi_app = ProxyFix(app.wsgi_app)
  
-if __name__ == '__main__':
+if __name__ == "__main__":
+  app.debug = "--debug" in sys.argv
   app.run()
